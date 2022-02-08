a9c3ea7858cc67de121475396d9b37fece88c1df
lelingling <997854977@qq.com>
2022-02-09 01:02:28 +0800

    工具页面更新

---
+++ personal-site/src/components/body/tool/toolContent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="toolContent">
+      <h4><img :src="titleSvg">站长工具</h4>
+  </div>
+</template>
+
+<script>
+import titleImg from '@/assets/image/header-life_pink.svg';
+export default {
+    data(){
+        return {
+            titleSvg :titleImg,
+        }
+    }
+}
+</script>
+
+<style scoped lang="less">
+#toolContent{
+    padding: 1rem 1.5rem;
+    width: 100%;
+    height: 100px;
+    h4{
+        font-size: 1.4rem;
+        img{
+            width:1.4rem;
+            height: 1.4rem;
+            line-height: 1.4rem;
+            vertical-align: middle;
+        }
+    }
+}
+
+</style>
